ff5f8e36baf0a40d82c4f6842263a75cffca5b83
Rafa Diaz <racrux@gmail.com>
2016-03-31 02:37:21 -0700

    Filling all the cards.

--- cartitas/cartitas.js
+++ cartitas/cartitas.js
@@ -28,24 +28,36 @@ var Card = (function () {
 
 function generate(n) {
   var deck = $("#deck");
-  var i, j;
+  var i, j, k;
 
   // Clear any previous deck
   deck.empty();
 
   // Create required cards
   var cards = [];
-  //for (i = 0; i < n * (n - 1) + 1; ++i) {
-  for (i = 0; i < n; ++i) {
+  for (i = 0; i < n * (n - 1) + 1; ++i) {
     cards.push(new Card(n));
   }
 
-  // Initialize first n cards
+  // Fill first n cards
   for (i = 0; i < n; ++i) {
     cards[i].push(n * (n - 1));
 
     for(j = 0; j < n - 1; ++j) {
       cards[i].push(i * (n - 1) + j);
+    }
+  }
+
+  // Fill remaining cards
+  for (i = 0; i < n - 1; ++i) {
+    for (j = 0; j < n - 1; ++j) {
+      // Locate the card to fill
+      var card = cards[n + i * (n - 1) + j];
+      card.push(i);
+      // Generate the n values for the given card
+      for (k = 1; k < n; ++k) {
+        card.push(k * (n - 1) + (i * k + j) % (n - 1));
+      }
     }
   }
 
@@ -59,6 +71,12 @@ $(function () {
   var nField = $("#n").val(initialValue)
 
   $("#generate").click(function () {
-    generate(nField.val());
+    generate(+nField.val());
+  });
+
+  nField.keypress(function (e) {
+    if (e.which == 13) {
+      generate(+nField.val());
+    }
   });
 });
